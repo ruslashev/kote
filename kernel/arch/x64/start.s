de5c1a384a32af1a50173d165624b26774e04e76
Ruslan Akbashev <ruslashev@gmail.com>
2021-08-25 03:48:34 +0300

    start.s: add some initial checks

--- kernel/arch/x64/start.s
+++ kernel/arch/x64/start.s
@@ -22,31 +22,35 @@ mb_end:
 %macro print 1
 	mov ecx, %%loop_start - %%strdata
 	mov eax, 0x0700
-	mov ebx, [text_y]
-	shl ebx, 1
 	jmp %%loop_start
 %%strdata: db %1
 %%loop_start:
 	mov al, [%%strdata + ecx - 1]
-	mov [0xb8000 + ecx * 2 + ebx - 2], ax
+	mov [0xb8000 + ecx * 2 - 2], ax
 	loop %%loop_start
-	add dword [text_y], 80
 %endmacro
 
-section .bootstrap
 bits 32
+section .bootstrap
 start:
 	; Clear interrupts
 	cli
 
+	; Set up stack
+	mov esp, init_stack
+
 	; Save multiboot info
 	mov [mb_info], ebx
+	push eax
 
-	; Clear screen
-	mov ecx, 2 * 80 * 25
-	xor eax, eax
-	mov edi, 0xb8000
-	rep stosw
+	call clear_screen
+
+	pop eax
+	call check_multiboot
+
+	call check_cpuid
+
+	call check_long_mode
 
 	print "Hello, World!"
 
@@ -54,8 +58,58 @@ hltspin:
 	hlt
 	jmp hltspin
 
+clear_screen:
+	mov ecx, 2 * 80 * 25
+	xor eax, eax
+	mov edi, 0xb8000
+	rep stosw
+	ret
+
+check_multiboot:
+	cmp eax, 0x36d76289
+	jne .fail
+	ret
+.fail:
+	print "Not loaded by a multiboot-compliant bootloader"
+	jmp hltspin
+
+check_cpuid:
+	pushfd
+	pop eax          ; eax = flags
+	mov ecx, eax     ; copy to compare and restore
+	xor eax, 1 << 21 ; flip a bit
+	push eax
+	popfd            ; write to flags
+	pushfd
+	pop eax          ; and read from flags
+	push ecx
+	popfd            ; restore old value
+	xor eax, ecx     ; check if the bit was, in fact, flipped
+	jz .fail
+	ret
+.fail:
+	print "CPUID instruction unavailable"
+	jmp hltspin
+
+check_long_mode:
+	mov eax, 0x80000000
+	cpuid
+	cmp eax, 0x80000001 ; check for extended functions
+	jbe .fail
+	mov eax, 0x80000001
+	cpuid
+	test edx, 1 << 29   ; long mode bit
+	jz .fail
+	ret
+.fail:
+	print "This CPU is not 64-bit capable"
+	jmp hltspin
+
+section .bss
+init_stack_bottom:
+	resb 4096 * 2
+init_stack:
+
 section .data
-text_y:
-	dd 0
 mb_info:
 	dq 0
