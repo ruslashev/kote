90860859cf9c2b89ffe98ddb7796432db1846109
Ruslan Akbashev <ruslashev@gmail.com>
2021-08-27 22:31:24 +0300

    panic: add panic_early

---
+++ kernel/consts.rs
@@ -0,0 +1,1 @@
+pub const KERNEL_BASE: u64 = 0xffffffff80000000;
--- kernel/main.rs
+++ kernel/main.rs
@@ -1,18 +1,12 @@
 #![no_std]
 
+mod consts;
 mod panic;
-
-const KERNEL_BASE: u64 = 0xffffffff80000000;
 
 #[no_mangle]
 pub fn kmain() -> !
 {
-	let vga = (KERNEL_BASE + 0xb8000) as *mut u16;
-
-	unsafe {
-		*vga.offset(0) = 0x0728;
-		*vga.offset(1) = 0x073a;
-	}
+	panic::panic_early("oops!");
 
 	loop {}
 }
