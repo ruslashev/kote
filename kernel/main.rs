ea303b0097fa14e3868d73805a5cb408a9540853
Ruslan Akbashev <ruslashev@gmail.com>
2021-08-27 03:43:23 +0300

    add rustfmt.toml
    subject to change

--- kernel/main.rs
+++ kernel/main.rs
@@ -7,18 +7,18 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_: &PanicInfo) -> !
 {
-    loop {}
+	loop {}
 }
 
 #[no_mangle]
 pub fn kmain() -> !
 {
-    let vga = (KERNEL_BASE + 0xb8000) as *mut u16;
+	let vga = (KERNEL_BASE + 0xb8000) as *mut u16;
 
-    unsafe {
-        *vga.offset(0) = 0x0728;
-        *vga.offset(1) = 0x073a;
-    }
+	unsafe {
+		*vga.offset(0) = 0x0728;
+		*vga.offset(1) = 0x073a;
+	}
 
-    loop {}
+	loop {}
 }
