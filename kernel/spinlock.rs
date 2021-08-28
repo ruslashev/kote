d31db413cdd1cff42a7690240a393064fd068c65
Ruslan Akbashev <ruslashev@gmail.com>
2021-08-28 08:10:32 +0300

    add a horrendous spinlock implementation

---
+++ kernel/spinlock.rs
@@ -0,0 +1,29 @@
+use core::sync::atomic::{AtomicBool, Ordering};
+
+pub struct Spinlock
+{
+	locked: AtomicBool,
+}
+
+impl Spinlock
+{
+	pub const fn new() -> Self
+	{
+		Spinlock
+		{
+			locked: AtomicBool::new(false),
+		}
+	}
+
+	pub fn lock(&self)
+	{
+		while self.locked.compare_exchange_weak(false, true, Ordering::Acquire, Ordering::Acquire).is_err() {
+			core::hint::spin_loop();
+		}
+	}
+
+	pub fn unlock(&self)
+	{
+		self.locked.store(false, Ordering::Release);
+	}
+}
