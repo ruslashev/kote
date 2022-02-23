f9a8dd6fecd667793bbb4f790b4bc1afc0e368a4
Ruslan Akbashev <ruslashev@gmail.com>
2022-02-23 19:42:08 +0300

    pic: disable all lines by default

--- kernel/arch/x64/interrupts/pic.rs
+++ kernel/arch/x64/interrupts/pic.rs
@@ -7,37 +7,72 @@ use crate::arch::io;
 const PIC_IRQ_OFFSET: u8 = 32;
 const PIC1: u16 = 0x20;
 const PIC2: u16 = 0xa0;
+const PIC1_DATA: u16 = PIC1 + 1;
+const PIC2_DATA: u16 = PIC2 + 1;
 
 /// Remap IRQs from 0..15 to 32..47 (+PIC_IRQ_OFFSET) to not conflict with CPU exceptions
 pub fn remap() {
-    let pic1_data = PIC1 + 1;
-    let pic2_data = PIC2 + 1;
-
     // Begin initialization
     outb_wait(PIC1, 0x11);
     outb_wait(PIC2, 0x11);
 
     // Tell offsets
-    outb_wait(pic1_data, PIC_IRQ_OFFSET);
-    outb_wait(pic2_data, PIC_IRQ_OFFSET + 8);
+    outb_wait(PIC1_DATA, PIC_IRQ_OFFSET);
+    outb_wait(PIC2_DATA, PIC_IRQ_OFFSET + 8);
 
     // Setup cascading
-    outb_wait(pic1_data, 4);
-    outb_wait(pic2_data, 2);
+    outb_wait(PIC1_DATA, 4);
+    outb_wait(PIC2_DATA, 2);
 
     // Set 8086 mode for some reason
-    outb_wait(pic1_data, 1);
-    outb_wait(pic2_data, 1);
+    outb_wait(PIC1_DATA, 1);
+    outb_wait(PIC2_DATA, 1);
 
-    // Clear masks
-    outb_wait(pic1_data, 0);
-    outb_wait(pic2_data, 0);
+    // Disable all lines
+    outb_wait(PIC1_DATA, 0xff);
+    outb_wait(PIC2_DATA, 0xff);
 }
 
 #[inline(always)]
 fn outb_wait(port: u16, val: u8) {
     io::outb(port, val);
     io::io_wait();
+}
+
+pub fn enable_line(irq: u8) {
+    let port;
+    let line;
+
+    if irq < 8 {
+        port = PIC1_DATA;
+        line = irq;
+    } else {
+        port = PIC2_DATA;
+        line = irq - 8;
+    }
+
+    let bit = 1 << line;
+    let value = io::inb(port) & !bit;
+
+    io::outb(port, value);
+}
+
+pub fn disable_line(irq: u8) {
+    let port;
+    let line;
+
+    if irq < 8 {
+        port = PIC1_DATA;
+        line = irq;
+    } else {
+        port = PIC2_DATA;
+        line = irq - 8;
+    }
+
+    let bit = 1 << line;
+    let value = io::inb(port) | bit;
+
+    io::outb(port, value);
 }
 
 /// Send end-of-interrupt
