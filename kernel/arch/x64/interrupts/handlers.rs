f7ecd098c76a8986a6ba322991bed43c347cc3a0
Ruslan Akbashev <ruslashev@gmail.com>
2022-07-31 19:30:08 +0300

    x64/handlers: print type of error on hitting stack guards

--- kernel/arch/x64/interrupts/handlers.rs
+++ kernel/arch/x64/interrupts/handlers.rs
@@ -2,7 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use crate::types::PowerOfTwoOps;
+
 use super::exceptions::ExceptionFrame;
+
+extern "C" {
+    fn stack_guard_top();
+    fn stack_guard_bot();
+}
 
 pub(super) fn divide_by_zero(_frame: &ExceptionFrame) {
     println!("Divide by zero handler");
@@ -10,6 +17,17 @@ pub(super) fn divide_by_zero(_frame: &ExceptionFrame) {
 
 pub(super) fn page_fault(_frame: &ExceptionFrame) {
     let vaddr = read_reg!(cr2);
+    let round = vaddr.page_round_down();
+    let guard_top_low = (stack_guard_top as u64) & 0xffffffff;
+    let guard_bot_low = (stack_guard_bot as u64) & 0xffffffff;
+
+    if round == guard_top_low {
+        panic!("Kernel stack overflow");
+    }
+
+    if round == guard_bot_low {
+        panic!("Kernel stack underflow");
+    }
 
     println!("Faulting addr = {:#x}", vaddr);
 }
