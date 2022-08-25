6c1768f76b9fef0cbf5e4f7bbb90c7576fd078b9
Ruslan Akbashev <ruslashev@gmail.com>
2022-08-25 20:16:30 +0300

    small_vec: rewrite as a ringbuffer
    This eliminates unsoundness wrt storing index to current process

--- kernel/sched.rs
+++ kernel/sched.rs
@@ -10,55 +10,54 @@ use crate::process::{Process, State};
 use crate::small_vec::SmallVec;
 use crate::spinlock::Mutex;
 
-static LOOP_ELF: &[u8] = include_bytes!("../build/loop");
-
 static SCHEDULER: Mutex<OnceCell<Scheduler>> = Mutex::new(OnceCell::new());
 
 struct Scheduler {
-    current_idx: usize,
     processes: SmallVec<Process>,
 }
 
 impl Scheduler {
     fn new() -> Self {
         Self {
-            current_idx: 0,
             processes: SmallVec::new(),
         }
     }
 
-    fn get_next(&self) -> TaskSwitch {
-        for proc in self.processes.iter_round_robin(self.current_idx) {
+    fn get_next(&mut self) -> TaskSwitch {
+        for (idx, proc) in self.processes.iter_round_robin() {
             if proc.state == State::Runnable {
-                return TaskSwitch::NewTask(*proc);
+                return TaskSwitch::NewTask(idx, *proc);
             }
         }
 
-        let current = self.processes[self.current_idx];
-
-        if current.state == State::Running {
-            return TaskSwitch::SameTask(current);
+        if let Some(current) = self.processes.current() && current.state == State::Running {
+            return TaskSwitch::SameTask(*current);
         }
 
         TaskSwitch::Idle
     }
 
-    fn preempt_current(&mut self) {
-        self.processes[self.current_idx].state = State::Runnable;
+    fn set_current(&mut self, new_idx: usize) {
+        if let Some(current) = self.processes.current() {
+            current.state = State::Runnable;
+        }
+
+        self.processes.set_current(new_idx);
     }
 }
 
 enum TaskSwitch {
-    NewTask(Process),
+    NewTask(usize, Process),
     SameTask(Process),
     Idle,
 }
 
 pub fn init(info: &BootloaderInfo) {
+    static LOOP_ELF: &[u8] = include_bytes!("../build/loop");
+
     let mut sched = Scheduler::new();
 
-    sched.processes.push(Process::from_elf(LOOP_ELF, info));
-    sched.processes[0].state = State::Running;
+    sched.processes.push_back(Process::from_elf(LOOP_ELF, info));
 
     assert!(SCHEDULER.lock().set(sched).is_ok());
 }
@@ -68,9 +67,9 @@ pub fn next() {
     let sched = cell.get_mut().unwrap();
 
     match sched.get_next() {
-        TaskSwitch::NewTask(proc) => {
-            sched.preempt_current();
-            run(proc)
+        TaskSwitch::NewTask(new_idx, proc) => {
+            sched.set_current(new_idx);
+            run(proc);
         }
         TaskSwitch::SameTask(proc) => run(proc),
         TaskSwitch::Idle => idle(),
--- kernel/small_vec.rs
+++ kernel/small_vec.rs
@@ -2,111 +2,134 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use core::ops::{Index, IndexMut};
-use core::{mem, slice};
+use core::mem;
 
 use crate::arch::mmu::PAGE_SIZE;
 use crate::mm::pg_alloc;
 
 pub struct SmallVec<T> {
-    storage: *mut T,
-    size: usize,
-    capacity: usize,
+    buf: *mut T,
+    cap: usize,
+    head: usize,
+    tail: usize,
+    view: usize,
 }
 
 impl<T> SmallVec<T> {
     pub fn new() -> Self {
         let page = pg_alloc::alloc_page();
-        let addr = page.to_physaddr().into_vaddr().0;
+        let addr = page.to_physaddr().into_vaddr();
+        let slice = unsafe { addr.into_slice_mut(PAGE_SIZE) };
+
+        Self::from_slice(slice)
+    }
+
+    pub fn from_slice<U>(slice: &mut [U]) -> Self {
+        let ptr = slice.as_mut_ptr();
+        let offset = ptr.align_offset(mem::align_of::<T>());
+
+        assert!(offset < slice.len());
+
+        let aligned = unsafe { ptr.add(offset) };
 
         Self {
-            storage: addr as *mut T,
-            size: 0,
-            capacity: PAGE_SIZE / mem::size_of::<T>(),
+            buf: aligned.cast::<T>(),
+            cap: (slice.len() - offset) / mem::size_of::<T>(),
+            head: 0,
+            tail: 0,
+            view: 0,
         }
     }
 
-    pub fn push(&mut self, item: T) {
-        assert!(self.size < self.capacity);
-
-        self.size += 1;
+    pub fn push_back(&mut self, item: T) {
+        assert!((self.tail + 1) % self.cap != self.head, "small_vec: overflow");
 
         unsafe {
-            self.last_element().write(item);
+            self.buf.add(self.tail).write(item);
+        }
+
+        self.tail += 1;
+        self.tail %= self.cap;
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        if self.head == self.tail {
+            return None;
+        }
+
+        let elem = unsafe { self.buf.add(self.head).read() };
+
+        self.head += 1;
+        self.head %= self.cap;
+
+        if self.view < self.head {
+            self.view = self.head;
+        }
+
+        Some(elem)
+    }
+
+    pub fn current(&self) -> Option<&mut T> {
+        if self.head == self.tail {
+            None
+        } else {
+            unsafe { self.buf.add(self.view).as_mut() }
         }
     }
 
-    pub fn delete_at(&mut self, idx: usize) {
-        assert!(self.size > 0 && idx < self.size);
+    pub fn set_current(&mut self, new_view: usize) {
+        assert!(new_view >= self.head && new_view < self.tail);
 
-        unsafe {
-            let del_ptr = self.storage.add(idx);
-
-            self.last_element().swap(del_ptr);
-
-            drop(self.last_element().read());
-        }
-
-        self.size -= 1;
+        self.view = new_view;
     }
 
-    unsafe fn last_element(&mut self) -> *mut T {
-        self.storage.add(self.size - 1)
-    }
+    pub fn iter_round_robin(&self) -> RoundRobinIterator<T> {
+        let len = if self.head < self.tail {
+            self.tail - self.head
+        } else {
+            self.tail + self.cap - self.head
+        };
 
-    pub fn iter_round_robin(&self, start: usize) -> RoundRobinIterator<T> {
         RoundRobinIterator {
-            index: start,
-            start,
-            smvec: self,
+            idx: self.view,
+            len,
+            vec: self,
         }
-    }
-}
-
-impl<T> Index<usize> for SmallVec<T> {
-    type Output = T;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        let slice = unsafe { slice::from_raw_parts(self.storage, self.size) };
-
-        &slice[index]
-    }
-}
-
-impl<T> IndexMut<usize> for SmallVec<T> {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        let slice_mut = unsafe { slice::from_raw_parts_mut(self.storage, self.size) };
-
-        &mut slice_mut[index]
     }
 }
 
 impl<T> Drop for SmallVec<T> {
     fn drop(&mut self) {
-        for i in 0..self.size {
-            let elem = unsafe { self.storage.add(i).read() };
+        while let Some(elem) = self.pop_front() {
             drop(elem);
         }
     }
 }
 
 pub struct RoundRobinIterator<'a, T> {
-    index: usize,
-    start: usize,
-    smvec: &'a SmallVec<T>,
+    idx: usize,
+    len: usize,
+    vec: &'a SmallVec<T>,
 }
 
 impl<'a, T> Iterator for RoundRobinIterator<'a, T> {
-    type Item = &'a T;
+    type Item = (usize, &'a T);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.index += 1;
-        self.index %= self.smvec.size;
-
-        if self.index == self.start {
+        if self.len == 0 {
             None
         } else {
-            Some(&self.smvec[self.index])
+            let idx = self.idx;
+            let elem = unsafe { self.vec.buf.add(idx).as_ref().unwrap() };
+
+            self.len -= 1;
+
+            self.idx += 1;
+            if self.idx == self.vec.tail {
+                self.idx = self.vec.head;
+            }
+
+            Some((idx, elem))
         }
     }
 }
