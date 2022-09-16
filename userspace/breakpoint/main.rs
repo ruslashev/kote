7fe18b59d67779d0ec26085938ab46385b9fc294
Ruslan Akbashev <ruslashev@gmail.com>
2022-09-17 00:05:50 +0300

    userspace: add interrupt-invoking program

---
+++ userspace/breakpoint/main.rs
@@ -0,0 +1,23 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+#![allow(clippy::empty_loop)]
+
+use core::arch::asm;
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    loop {
+        unsafe {
+            asm!("int3");
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
