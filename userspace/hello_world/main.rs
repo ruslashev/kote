d77d4c3d97427ca0d96d2d26545fc56f0b332afd
Ruslan Akbashev <ruslashev@gmail.com>
2022-10-06 03:58:02 +0300

    userspace: add hello_world program

---
+++ userspace/hello_world/main.rs
@@ -0,0 +1,43 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+#![allow(clippy::empty_loop)]
+
+use core::arch::asm;
+
+static STR: &str = "Hello, World!";
+
+fn syscall(num: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64) -> u64 {
+    let ret;
+
+    unsafe {
+        asm!(
+            "syscall",
+            inlateout("rax") num => ret,
+            in("rdi") arg1,
+            in("rsi") arg2,
+            in("rdx") arg3,
+            in("r10") arg4,
+            out("rcx") _,
+            out("r11") _,
+            options(nostack),
+        );
+    }
+
+    ret
+}
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    loop {
+        syscall(1, STR.as_ptr() as u64, STR.len() as u64, 0, 0);
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
